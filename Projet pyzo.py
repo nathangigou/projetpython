c5fb169720a7ac6ed16ed672f2ad67eff113536c
Flantier29 <carl.le-gall@eivp-paris.fr>
2020-11-15 17:09:24 +0100

    Fonction conversion colonnes sent_at en liste datetime

--- Projet pyzo.py
+++ Projet pyzo.py
@@ -2,11 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+import datetime
 
 data = pd.read_csv("/Users/carll/Documents/EIVP/IVP1/Python/Projet/EIVP_KM.csv",delimiter=";")
 
-x=data[data.id==1].sent_at
+
+def Converttime(n):
+    L=len(data[data.id==n].sent_at)
+    # Nb de mesures prises par le capteur n
+    l=[]
+    # Création de la liste des dates
+    for i in range(L):
+        l.append(datetime.datetime.strptime(data.sent_at.loc[i], "%Y-%m-%d %H:%M:%S %z"))
+        # Ajout à la liste la chaine de caractère sent_at convertie en un objet datetime
+    return l
+
+x=matplotlib.dates.date2num(ld)
 y=data[data.id==1].lum
 
-data.plot(x,y)
+matplotlib.pyplot.plot_date(x,y)
+plt.scatter(x,y,s=9,c='red')
 plt.show()
