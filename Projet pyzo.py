b7daa74e96cf1a4f4ada96b89a826dfa390db5aa
nathan.gigou <nathan.gigou@eivp-paris.fr>
2020-11-15 19:11:04 +0100

    chgts

--- Projet pyzo.py
+++ Projet pyzo.py
@@ -4,18 +4,18 @@ import matplotlib
 from matplotlib import pyplot as plt
 import datetime
 
-data = pd.read_csv("/Users/carll/Documents/EIVP/IVP1/Python/Projet/EIVP_KM.csv",delimiter=";")
+data = pd.read_csv("/Users/natha/Documents/EIVP/Algorithmique/Projet python/projetpython/EIVP_KM.csv",delimiter=";")
 
 
-# def Converttime(n):
-#     L=len(data[data.id==n].sent_at)
-#     # Nb de mesures prises par le capteur n
-#     l=[]
-#     # Création de la liste des dates
-#     for i in range(L):
-#         l.append(datetime.datetime.strptime(data.sent_at.loc[i+data[data.id==n].index[0]], "%Y-%m-%d %H:%M:%S %z"))
-#         # Ajout à la liste la chaine de caractère sent_at convertie en un objet datetime
-#     return l
+def Converttime(n):
+     L=len(data[data.id==n].sent_at)
+     # Nb de mesures prises par le capteur n
+     l=[]
+     # Création de la liste des dates
+     for i in range(L):
+         l.append(datetime.datetime.strptime(data.sent_at.loc[i+data[data.id==n].index[0]], "%Y-%m-%d %H:%M:%S %z"))
+         # Ajout à la liste la chaine de caractère sent_at convertie en un objet datetime
+     return l
 
 def courbes(v,capteur):
     ld=Converttime(capteur)
@@ -24,8 +24,8 @@ def courbes(v,capteur):
     matplotlib.pyplot.plot_date(x,y)
     plt.show()
 
-# ld=Converttime(1)
-# x=matplotlib.dates.date2num(ld)
-# y=data[data.id==1].lum
-# matplotlib.pyplot.plot_date(x,y)
-# plt.show()
+ld=Converttime(1)
+x=matplotlib.dates.date2num(ld)
+y=data[data.id==1].lum
+matplotlib.pyplot.plot_date(x,y)
+plt.show()
