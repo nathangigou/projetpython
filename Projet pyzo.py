454cbdc81371ed323b75745090e13828cc9086c9
Flantier29 <carl.le-gall@eivp-paris.fr>
2020-11-15 18:19:01 +0100

    Réglage erreur sur fct Converttime & tentative fct courbes avec en paramètres une variables et le capteur souhaité

--- Projet pyzo.py
+++ Projet pyzo.py
@@ -7,19 +7,25 @@ import datetime
 data = pd.read_csv("/Users/carll/Documents/EIVP/IVP1/Python/Projet/EIVP_KM.csv",delimiter=";")
 
 
-def Converttime(n):
-    L=len(data[data.id==n].sent_at)
-    # Nb de mesures prises par le capteur n
-    l=[]
-    # Création de la liste des dates
-    for i in range(L):
-        l.append(datetime.datetime.strptime(data.sent_at.loc[i], "%Y-%m-%d %H:%M:%S %z"))
-        # Ajout à la liste la chaine de caractère sent_at convertie en un objet datetime
-    return l
+# def Converttime(n):
+#     L=len(data[data.id==n].sent_at)
+#     # Nb de mesures prises par le capteur n
+#     l=[]
+#     # Création de la liste des dates
+#     for i in range(L):
+#         l.append(datetime.datetime.strptime(data.sent_at.loc[i+data[data.id==n].index[0]], "%Y-%m-%d %H:%M:%S %z"))
+#         # Ajout à la liste la chaine de caractère sent_at convertie en un objet datetime
+#     return l
 
-x=matplotlib.dates.date2num(ld)
-y=data[data.id==1].lum
+def courbes(v,capteur):
+    ld=Converttime(capteur)
+    x=matplotlib.dates.date2num(ld)
+    y=data[data.id==capteur].data
+    matplotlib.pyplot.plot_date(x,y)
+    plt.show()
 
-matplotlib.pyplot.plot_date(x,y)
-plt.scatter(x,y,s=9,c='red')
-plt.show()
+# ld=Converttime(1)
+# x=matplotlib.dates.date2num(ld)
+# y=data[data.id==1].lum
+# matplotlib.pyplot.plot_date(x,y)
+# plt.show()
